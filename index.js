b48c7e7ad541c5f25ccb7273d51bcd52fe5e05c7
kailu90 <czuluaga2018@gmail.com>
2023-11-06 20:18:33 -0500

    Se modifica el logo, se retira del header en el centro y se incluye en el menú mobile

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-/*Función para mover el slider con las imágenes, para desplazar 
-imágenes a la izquierda y a la derecha*/
-
-const FlechaIzquierdaSlider = document.querySelector(".flecha_izquierda_slider");
-const FlechaDerechaSlider = document.querySelector(".flecha_derecha_slider");
-const slider = document.querySelector("#slider");
-const sliderItem = document.querySelectorAll(".slider_item");
-
-FlechaDerechaSlider.addEventListener("click", e => moverDerecha() )
-FlechaIzquierdaSlider.addEventListener("click", e => moverIzquierda())
-
-let posicion = 0;
-let lenghtImage = 100 / sliderItem.length;
-
-function moverDerecha() { 
-    console.log(lenghtImage)
-    posicion = posicion + lenghtImage;
-    slider.style.transform = 'translate(-${posicion}%)'
-    slider.style.transition = "all ease 0.6s"
-}
-
-function moverIzquierda() {
-    console.log("moverIzquierda")
-    posicion = posicion - lenghtImage;
-    slider.style.transform = 'translate(${posicion}%)'
-    slider.style.transition = "all ease 0.6s"
-}
-
 /*Función para mostrar el menú mobile*/
 
 const menuHamburguesa = document.querySelector(".menu_hamburguesa");
